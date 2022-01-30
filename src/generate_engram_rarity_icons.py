7880773b02006236f97f9ff6eb154aa3a00f149e
FYWinds <i@windis.cn>
2022-01-30 22:11:26 +0800

    :new: 记忆水晶图标

--- src/generate_engram_rarity_icons.py
+++ src/generate_engram_rarity_icons.py
@@ -1,3 +1,22 @@
 from tools import writeFile
 from manifest import get, getAll, loadLocal
 from data.generated_enums import ItemCategoryHashes
+
+loadLocal()
+
+results: dict[str, str] = {}
+for i in getAll("DestinyInventoryItemDefinition"):
+    if (
+        (i["inventory"].get("tierTypeName") not in results)
+        and (ItemCategoryHashes.记忆水晶 in i.get("itemCategoryHashes", []))
+        and (
+            str(i["displayProperties"]["name"]).startswith(
+                i["inventory"]["tierTypeName"]
+            )
+        )
+        and (get("DestinyVendorDefinition", i["hash"]) is not None)
+    ):
+        print(i)
+        results[i["inventory"]["tierTypeName"]] = i["displayProperties"]["icon"]
+
+writeFile("./output/engram-rarity-icons.json", results)
