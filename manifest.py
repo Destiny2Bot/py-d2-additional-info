638dcf4bce897a7be83e5310238ffd9d84c02177
Tian-que <1605206150@qq.com>
2022-01-30 13:52:05 +0800

    :construction: 支持多语言 manifest

--- manifest.py
+++ manifest.py
@@ -4,21 +4,23 @@ import ujson
 
 from tools import readFile
 
-BungieJSONFilePath = "./data/BungieManifest/BungieJSON.json"
+BungieJSONFilePath = "./data/BungieManifest/"
 
-manifest_json: Dict[Any, Any] = {}
+manifest_json: Dict[str, Dict[Any, Any]] = {}
 
 
-def loadLocal() -> None:
+def loadLocal(language: str = "zh-chs") -> None:
     global manifest_json
-    manifest_json = ujson.loads(readFile(BungieJSONFilePath))
+    manifest_json[language] = ujson.loads(
+        readFile(BungieJSONFilePath + f"BungieJSON_{language}.json")
+    )
 
 
-def getAll(tablename: str = None) -> dict:
+def getAll(tablename: str = None, language: str = "zh-chs") -> dict:
     if tablename:
-        return manifest_json[tablename].values()
+        return manifest_json[language][tablename].values()
     return manifest_json
 
 
-def get(tablename: str, hash: Union[str, int]) -> dict:
-    return manifest_json[tablename].get(str(hash))
+def get(tablename: str, hash: Union[str, int], language: str = "zh-chs") -> dict:
+    return manifest_json[language][tablename].get(str(hash))
