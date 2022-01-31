6bfbf34c677c843f0257c2e5d01fbe0eb231a046
Tian-que <1605206150@qq.com>
2022-01-31 15:41:23 +0800

    :book: 添加新注释

--- config.py
+++ config.py
@@ -6,9 +6,16 @@ class Settings(BaseSettings):
     pgurl: PostgresDsn = Field(
         default="postgresql://postgres:postgres@localhost:5432/postgres"
     )
+    """Postgres数据库链接信息"""
+
     api_key: str = ""
+    """Bungie API Key"""
+
     client_secret: str = ""
+    """Bungie Client Secret"""
+
     log_level: str = "INFO"
+    """日志级别"""
 
     class Config:
         env_file = ".env"
