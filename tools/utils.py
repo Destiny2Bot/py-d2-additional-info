dcec308a5b4ecf95b3e7fffd10bc7a61c68d46ac
FYWinds <i@windis.cn>
2022-01-31 17:20:13 +0800

    :memo: edit docstring

--- tools/utils.py
+++ tools/utils.py
@@ -12,13 +12,13 @@ def deduplicate(
     > 去除列表中的重复项
 
     :参数:
-      * `items: List[T]`: 列表
+        * `items: List[T]`: 列表
 
     :可选参数:
-      * `key: Callable[[T], Hashable] = None`: 如果有Unhashable元素，使用此函数将其转为Hashable
+        * `key: Callable[[T], Hashable] = None`: 如果有Unhashable元素，使用此函数将其转为Hashable
 
     :生成:
-      * `Generator[T, None, None]`: 去重后的列表
+        * `Generator[T, None, None]`: 去重后的列表
     """
     d = set()
     for item in items:
@@ -38,6 +38,19 @@ def dedupeAndSortArray(items: List[Sortable], reverse: bool = False) -> List[Sor
 
 
 def diffArrays(all: List[T], exclude: Optional[List[T]] = None) -> List[T]:
+    """
+    :说明: `diffArrays`
+    > [summary]
+
+    :参数:
+        * `all: List[T]`: [description]
+
+    :可选参数:
+        * `exclude: Optional[List[T]] = None`: [description]
+
+    :返回:
+        - `List[T]`: [description]
+    """
     if exclude is None:
         return list(deduplicate(all))
     return [i for i in deduplicate(all) if i not in exclude]
