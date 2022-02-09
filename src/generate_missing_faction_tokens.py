fbcb2e3c696c7555e82a83c542e50763f081efdf
Tian-que <1605206150@qq.com>
2022-02-09 12:05:59 +0800

    :new: generate_missing_faction_tokens未指定道具派系与NPC信息

---
+++ src/generate_missing_faction_tokens.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from tools import writeFile, dedupeAndSortArray
+from manifest import getAll, loadLocal
+
+loadLocal()
+
+inventoryItems = getAll("DestinyInventoryItemDefinition")
+factions = getAll("DestinyFactionDefinition")
+
+allTokenHashes: List = []
+for i in [
+    list(tokenValues.keys())
+    for faction in factions
+    if (tokenValues := faction.get("tokenValues"))
+]:
+    allTokenHashes.extend(i)
+allTokenHashes = [int(i) for i in allTokenHashes]
+allTokenHashes = dedupeAndSortArray(allTokenHashes)
+
+missingTokenHashes: List[int] = []
+badVendors: List[int] = []
+for hash in allTokenHashes:
+    item = [item for item in inventoryItems if item["hash"] == hash]
+    if not item:
+        missingTokenHashes.append(hash)
+        vendor = [i for i in factions if hash in i.get("tokenValues", {}).keys()]
+        if vendor:
+            badVendors.append(vendor[0]["hash"])
+
+# 输出没有指定库存物品的派系 tokenValues
+writeFile("./output/missing-faction-tokens.json", missingTokenHashes)
+# 输出没有指定库存物品的派系 tokenValues 的NPC hash
+writeFile("./output/bad-vendors.json", badVendors)
