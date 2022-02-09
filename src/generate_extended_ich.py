4c8477766e689e9a0cb720ac539b3f07b43c132c
Tian-que <1605206150@qq.com>
2022-02-09 17:42:05 +0800

    :new: generate_extended_ich 所有榴弹发射器和精密框架霰弹枪的 hash

---
+++ src/generate_extended_ich.py
@@ -0,0 +1,39 @@
+from tools import writeFile, sortObject
+from manifest import getAll, loadLocal
+from data.generated_enums import ItemCategoryHashes
+
+loadLocal()
+
+inventoryItems = getAll("DestinyInventoryItemDefinition")
+
+ffGrenadeLaunchers = [
+    item
+    for item in inventoryItems
+    if (itemCategoryHashes := item.get("itemCategoryHashes"))
+    and ItemCategoryHashes.榴弹发射器 in itemCategoryHashes
+    and ItemCategoryHashes.威能武器 not in itemCategoryHashes
+    and ItemCategoryHashes.样品模型 not in itemCategoryHashes
+]
+
+slugShotguns = [
+    item
+    for item in inventoryItems
+    if (itemCategoryHashes := item.get("itemCategoryHashes"))
+    and ItemCategoryHashes.霰弹枪 in itemCategoryHashes
+    and ItemCategoryHashes.样品模型 not in itemCategoryHashes
+    and (sockets := item.get("sockets"))
+    and sockets["socketEntries"][0].get("singleInitialItemHash") == 918679156  # 精密框架
+]
+
+extendedICH: dict[int, int] = {}
+
+for gl in ffGrenadeLaunchers:
+    extendedICH[gl["hash"]] = -ItemCategoryHashes.榴弹发射器
+
+for ssg in slugShotguns:
+    extendedICH[ssg["hash"]] = -ItemCategoryHashes.霰弹枪
+
+extendedICH = sortObject(extendedICH)
+
+# 所有榴弹发射器和精密框架霰弹枪的物品 hash
+writeFile("./output/extended-ich.json", extendedICH)
